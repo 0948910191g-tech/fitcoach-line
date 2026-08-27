7027fb389c991a32c383997031d5b6dc512f0846
0948910191g-tech <0948910191g@gmail.com>
2026-08-27 22:30:47 +0700

    feat: export durable worker runtime

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,1 +1,14 @@
-export {};
+export {
+  OWNER_ALPHA_WORKER_POLICY,
+  createSupabaseAIJobStore,
+  processNextAIJob,
+} from './process-job';
+export type {
+  AIJobFailure,
+  AIJobFailureOutcome,
+  AIJobStore,
+  ClaimedAIJob,
+  ProcessAIJobResult,
+  ProcessNextAIJobOptions,
+  WorkerPolicy,
+} from './process-job';
